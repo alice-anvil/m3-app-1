0843f88d749f1afd3a8e5e121fddbe92a127e7e7
alice@anvil.works <alice@anvil.works>
2023-09-26 15:43:53 +0000

    Edited settings and ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -2,7 +2,8 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.server
+from datetime import datetime
 
 @anvil.server.callable
 def record_name(name):
-  app_tables.visitors.add_row(name=name)
+  app_tables.visitors.add_row(name=name, when=datetime.now())
